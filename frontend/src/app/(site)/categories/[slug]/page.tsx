a3ec649b66b942e80cc949a343cc4e35c71b39ec
micas-tsx <micaelabud21@gmail.com>
2025-08-15 07:39:40 -0300

    feat: create the estruct of products page

--- frontend/src/app/(site)/categories/[slug]/page.tsx
+++ frontend/src/app/(site)/categories/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 type Props = {
   params: Promise<{ slug: string }>
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
@@ -11,7 +13,26 @@ export default async function Page({ params, searchParams }: Props) {
 
   return (
     <div className="">
-      slug: {slug}
+      <div className="text-gray-500 mb-4">
+        <Link href={'/'}>Home</Link> &gt; Temporário
+      </div>
+
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-center">
+        <div className="text-3xl"> <strong>99</strong> Produtos </div>
+        <div className="">...</div>
+      </div>
+
+      <div className="flex flex-col md:flex-row gap-8 mt-8">
+        <div className="flex-1 md:max-w-70">
+          Filtro
+        </div>
+        <div className="flex-1 grid grid-cols-1 md:grid-cols-3">
+          <div>...</div>
+          <div>...</div>
+          <div>...</div>
+          <div>...</div>
+        </div>
+        </div>
     </div>
   )
 }
