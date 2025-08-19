5c26e740563ca4cf54aaf1ed638ec2786b403291
micas-tsx <micaelabud21@gmail.com>
2025-08-18 22:07:52 -0300

    feat: create hydration, server functions, cookies of authorization login user

--- frontend/src/libs/server-cookies.ts
+++ frontend/src/libs/server-cookies.ts
@@ -1,6 +1,23 @@
 import { CartItem } from "@/types/cart-item"
 import { cookies } from 'next/headers'
 
+// AUTH COOKIE
+export const getServerAuthToken = async () => {
+  const cookieStore = await cookies()
+  return cookieStore.get('auth_token')?.value || null
+}
+
+export const setServerAuthToken = async (token: string) => {
+  const cookieStore = await cookies()
+  cookieStore.set('auth_token', token, { httpOnly: true })
+}
+
+export const clearServerAuthToken = async () => {
+  const cookieStore = await cookies()
+  cookieStore.delete('auth_token')
+} 
+
+// CART COOKIE
 export const getServerCart = async (): Promise<CartItem[]> => {
   const cookieStore = await cookies() // pega os cookies
   const value = cookieStore.get('cart')?.value //passa se tem valores nos cookies
@@ -9,7 +26,7 @@ export const getServerCart = async (): Promise<CartItem[]> => {
   try { //tenta {
     return JSON.parse(value) // retornar o valor como json
   } catch {
-    return [] // sen n retornar um array vazio
+    return [] // sen n: retornar um array vazio
   }
 }
 
--- frontend/src/providers/store-hydration.tsx
+++ frontend/src/providers/store-hydration.tsx
@@ -1,11 +1,23 @@
 "use client"
 
 import { useCartStore } from "@/store/cart"
+import { useAuthStore } from '@/store/auth'
 import { getCartState } from '@/actions/get-cart-state'
+import { getAuthState } from '@/actions/get-auth-state'
 import { useEffect } from 'react'
 
 export const StoreHydration = () => {
+  const authStore = useAuthStore(state => state)
+  
+  // Hydrate auth state
   useEffect(() => {
+    getAuthState().then(( token ) => {
+      if(token) authStore.setToken(token)  
+      authStore.setHydrated(true)
+    })
+
+
+    // hydrate cart state
     getCartState().then(({ cart }) => {
         if(cart.length > 0) {
           useCartStore.setState({ cart })
