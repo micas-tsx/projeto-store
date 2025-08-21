b9f8b2bc6909aa7b8a4d91001e6065eb29d8ad2b
micas-tsx <micaelabud21@gmail.com>
2025-08-21 18:21:40 -0300

    feat: get product data on his page

--- frontend/src/components/home/most-sold-products.tsx
+++ frontend/src/components/home/most-sold-products.tsx
@@ -12,7 +12,6 @@ export const MostSoldProducts = async () => {
       <h2 className="text-2xl text-center md:text-left">Produtos mais vendidos</h2>
       <p className="text-gray-500 text-center md:text-left">Campeões de vendas na nossa loja.</p>
 
-
       <div className="mt-9">
         <ProductList list={products} />
       </div>
--- frontend/src/components/product/related-products.tsx
+++ frontend/src/components/product/related-products.tsx
@@ -1,17 +1,19 @@
 import { ProductList } from "../product-list"
-import { data } from '@/data'
+import { getRelatedProducts } from '@/actions/get-related-products'
 
 type Props = {
   id: number
 }
 
-export const RelatedProducts = ({ id }: Props) => {
+export const RelatedProducts = async ({ id }: Props) => {
+  const products = await getRelatedProducts(id)
+
   return(
     <div className="mt-10">
       <h3 className="text-2xl">Você também vai gostar</h3>
 
       <div className="mt-9">
-        <ProductList list={data.products} />
+        <ProductList list={products} />
       </div>
     </div>
   )
